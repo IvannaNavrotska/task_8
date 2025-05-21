8f578098617d453e0b4b7303d1ad5e0fe2b32c56
Ivanna Navrotska <navrockaivanna15@gmail.com>
2025-05-21 13:40:08 +0300

    sort string

--- bwt.py
+++ bwt.py
@@ -1,10 +1,14 @@
 def BWT(string):
 
+    matrix = []
+    
     l_string = [] #перетворюємо вхідний рядок на список його символів
 
     for i in string:
         l_string.append(i)
-
-    return l_string
+        
+    sorted_ = sorted(l_string) #розміщуємо в лексикографічному порядку
+    
+    return sorted_
 
 print(BWT('owl'))
