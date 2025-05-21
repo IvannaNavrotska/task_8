cdf4b1191d8c43b12f3e17964e61648394f2e476
Ivanna Navrotska <navrockaivanna15@gmail.com>
2025-05-21 14:40:55 +0300

    bwt is ready for a text file

--- bwt.py
+++ bwt.py
@@ -3,16 +3,17 @@ def BWT(string):
     matrix = []
     
     l_string = [] #перетворюємо вхідний рядок на список його символів
-
     for i in string:
         l_string.append(i)
     
-    for j in range(len(l_string)):
+    for j in range(len(l_string)): #формуємо матрицю з усіх циклічних зсувів
         l_string = [l_string[-1]] + l_string[:-1]
         matrix.append(l_string[:])
         
-    return sorted(matrix)
+    sorted_matrix = sorted(matrix) #сортуємо матрицю циклічних зсувів
 
-res = BWT('AGGTCAACC$')
-for i in res:
-    print(i)
+    result = ''.join(row[-1] for row in sorted_matrix)
+
+    return result
+
+print(BWT('AGGTCAACC$'))
