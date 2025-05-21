671618cd8854b3d99f239e726a217638b00a519f
Ivanna Navrotska <navrockaivanna15@gmail.com>
2025-05-21 13:35:27 +0300

    string to list

--- bwt.py
+++ bwt.py
@@ -1,1 +1,10 @@
-print('hello bwt')
+def BWT(string):
+
+    l_string = [] #перетворюємо вхідний рядок на список його символів
+
+    for i in string:
+        l_string.append(i)
+
+    return l_string
+
+print(BWT('owl'))
