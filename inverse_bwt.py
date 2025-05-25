e5f895a420942d454b2c43cc28e63927079e1c47
Ivanna Navrotska <navrockaivanna15@gmail.com>
2025-05-25 13:56:05 +0300

    add better inverse bwt

--- inverse_bwt.py
+++ inverse_bwt.py
@@ -21,8 +21,50 @@ def inverse_BWT(string):
         result = sorted(result)
         result = [m + j for m, j in zip(l_string, result)]
 
-    result = sorted(result)
+    result = sorted(result) #$ на початку, а не в кінці!
     
     return result[0]
 
-print(inverse_BWT('сннааа'))
+print(inverse_BWT('AC$GATCTG')) 
+
+
+def better_inverse_BWT(string): 
+
+    symbol = string.index('$')  
+
+    l_string = list(string)
+    n = len(l_string)
+    sorted_l_string = sorted(l_string)
+
+    counts = {}
+    ranks_bwt = []
+    for i in l_string:
+        if i not in counts:
+            counts[i] = 0
+        ranks_bwt.append((i, counts[i]))
+        counts[i] += 1
+
+    counts = {}
+    ranks_sorted = []
+    for i in sorted_l_string:
+        if i not in counts:
+            counts[i] = 0
+        ranks_sorted.append((i, counts[i]))
+        counts[i] += 1
+
+    T = [0] * n
+    for i in range(n):
+        for j in range(n):
+            if ranks_bwt[i] == ranks_sorted[j]:
+                T[i] = j
+                break
+
+    result = []
+    pos = symbol
+    for _ in range(n):
+        result.append(string[pos])  
+        pos = T[pos]
+
+    return ''.join(reversed(result))
+
+print(better_inverse_BWT('AC$GATCTG'))  
